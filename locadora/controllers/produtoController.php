ac62b7d6926a6b4dac0655a8f1d4dbf7328f2861
fernando_dimas <fernando_dimas@10.1.105.61>
2014-10-21 22:09:57 -0200

    criação do produto

---
+++ locadora/controllers/produtoController.php
@@ -0,0 +1,88 @@
+<?php
+class produtoController extends CI_Controller{
+
+    public function index(){
+        $this->listar();
+    }
+    
+    public function listar(){
+        $this->load->model('produtoModel');
+        $pagina = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
+        $dados = array(
+            'produtos' => $this->produtodoModel->listarTudo(numRegPagina(), $pagina), 
+            'paginacao' => criaPaginacao(
+            'produtoController', $this->produtodoModel->contarTudo(), $this->uri->segment(3), 3),
+            'titulo' => "Lista de Produtos");
+        $this->load->view('produtoView', $dados);
+    }
+    
+    function novo(){
+        $this->load->model('produtodoModel');
+        $dados['titulo'] = "Cadastro de Produtos";
+        $this->load->view('produtoFormView', $dados);
+    }
+    function inserir_produto(){
+        $this->load->model('produtoModel');
+        
+        $inf = array(
+            'codigo' => $this->input->post('codigo'),
+            'nome' => $this->input->post('nome'),
+            'quantidade' => $this->input->post('quantidade'),
+            'quantidade_locado' => $this->input->post('quantidade_locado'),
+            'quantidade_disponivel' => $this->input->post('quantidade_disponivel'),
+            'valor' => $this->input->post('valor'),
+            'inativo' => $this->input->post('inativo'),
+            'data_cadastro' => $this->input->post('data_cadastro'),
+            'numero_serie' => $this->input->post('numero_serie'),
+            'detalhes' => $this->input->post('detalhes'),
+            'genero_codigo' => $this->input->post('genero_codigo'),
+            'classificacao_codigo' => $this->input->post('classificacao_codigo'),
+            'tipo_codigo' => $this->input->post('tipo_codigo'),
+            'dias_devolucao' => $this->input->post('dias_devolucao'),
+        );
+        if($this->produtoModel->inserir($inf)){
+            $this->session->set_flashdata('msg','Criado com sucesso!');
+            redirect('produtoController/index');
+        }else{
+            $this->session->set_flashdata('msg','Não consegui gravar!');
+        }
+    }
+    
+    function alterar_produto($codigo){
+        $this->load->model('produtoModel');
+        $dados['titulo'] = "Alteração de Produtos";
+        $dados['produto'] = $this->produtoModel->buscar_pelo_codigo($codigo);
+        
+        $this->form_validation->set_rules('codigo', 'codigo', 'trim');
+        $this->form_validation->set_rules('nome', 'nome', 'trim');
+        $this->form_validation->set_rules('quantidade', 'quantidade', 'trim');
+        $this->form_validation->set_rules('quantidade_locado', 'quantidade_locado', 'trim');
+        $this->form_validation->set_rules('quantidade_disponivel', 'quantidade_disponivel', 'trim');
+        $this->form_validation->set_rules('valor', 'valor', 'trim');
+        $this->form_validation->set_rules('inativo', 'inativo', 'trim');
+        $this->form_validation->set_rules('data_cadastro', 'data_cadastro', 'trim');
+        $this->form_validation->set_rules('numero_serie', 'numero_serie', 'trim');
+        $this->form_validation->set_rules('detalhes', 'detalhes', 'trim');
+        $this->form_validation->set_rules('genero_codigo', 'genero_codigo', 'trim');
+        $this->form_validation->set_rules('classificacao_codigo', 'classificacao_codigo', 'trim');
+        $this->form_validation->set_rules('tipo_codigo', 'tipo_codigo', 'trim');
+        $this->form_validation->set_rules('dias_devolucao', 'dias_devolucao', 'trim');
+        if($this->form_validation->run()){
+            $_POST['codigo'] = $codigo;
+            if($this->produtoModel->alterar($_POST)){
+                $this->session->set_flashdata('msg', 'Alterado com sucesso!');
+                redirect('produtoController/index');
+            }
+        }
+        $this->load->view('produtoUpdateView', $dados);
+    }
+    function eliminar_produto(){
+        $this->load->model('produtoModel');
+            $del = $this->produtoModel->eliminar();
+            if ($del > 0){
+                $this->session->set_flashdata('msg', 'Eliminado com sucesso!');
+                redirect('produtoController/index.php');
+            }
+        }
+    }
+
---
+++ locadora/views/produtoFormView.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>  
+
+        <!-- JQUERY --> 
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script> 
+        <script>window.jQuery || document.write('<script src="js/jquery-1.7.1.min.js"><\/script>')</script> 
+        <!-- TWITTER BOOTSTRAP CSS --> 
+        
+        <link href="<?php echo base_url('locadora/views/css/bootstrap.css') ?>" rel="stylesheet" type="text/css"/>  
+        <link href="<?php echo base_url('locadora/views/css/estilo.css') ?>" rel="stylesheet" type="text/css"/>
+        <!-- TWITTER BOOTSTRAP JS --> 
+        <script src="<?php echo base_url('locadora/views/jquery/js/bootstrap.min.js') ?>"></script>       
+        <script src="<?php echo base_url('locadora/views/jquery/css/ui-lightness/jquery-ui-1.10.4.custom.css') ?>"></script>       
+        <script type="text/javascript" src="<?php echo base_url('locadora/views/jquery/js/jquery-1.9.1.js') ?>"></script>
+        <script type="text/javascript" src="<?php echo base_url('locadora/views/jquery/js/jquery-ui.js') ?>"></script>
+        <script type="text/javascript" src="<?php echo base_url('locadora/views/jquery/js/jquery.maskMoney.js') ?>"></script>
+        <script type="text/javascript">
+            $(document).ready(function(e) {
+                $("#datepicker").datepicker({
+                    dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
+                    dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
+                    dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+                    monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+                    montNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+                    dateFormat: 'dd/mm/yy',
+                    nextText: 'Próximo',
+                    prevText: 'Anterior'
+
+                });
+            });
+
+        </script>    
+    </head>
+    <body>
+        <!-- HEADER --> 
+        <header class="container-fluid"> 
+            <div class="row-fluid"> 
+                <div class="span12"> 
+                    <div class="navbar"> 
+                        <div class="navbar-inner"> 
+                            <div class="container"> 
+                                <h2 class="center"><?php echo $titulo; ?></h2> 
+                            </div> 
+                        </div> 
+                    </div> 
+                </div> 
+            </div> 
+        </header> 
+        <!-- / HEADER -->
+        <div class="container-fluid"> 
+            <!-- CLASSE PARA DEFINIR UMA LINHA --> 
+            <div class="row-fluid"> 
+                <!-- COLUNA OCUPANDO 2 ESPAÇOS NO GRID -->         
+                <div class="span12">
+                    <div class="well">
+
+
+
+                        <?php
+                        if ($this->session->flashdata('msg')) {
+                            echo $this->session->flashdata('msg');
+                        }
+
+                        echo form_open('produtoController/inserir_cliente');
+                        echo form_fieldset('Informações dos Produtos');
+                        ?>
+                        <form class="form-horizontal">
+                            <div class="control-group">
+                                <?php
+                                echo form_label("Nome: ");
+                                echo form_input('nome', '', 'size="50" class="campo" required');
+                                echo br();
+
+                                echo form_label("Quantidade: ");
+                                echo form_input('quantidade', '', 'size="5" class="campo"');
+                                echo br();
+
+                                echo form_label("Quantidade Locado: ");
+                                echo form_input('quantidade_locado', '', 'size="5" class="campo" required');
+                                echo br();
+
+                                echo form_label("Quantidade Disponivel: ");
+                                echo form_input('quantidade_disponivel', '', 'size="10" class="campo" required');
+                                echo br();
+
+                                echo form_label("Valor: ");
+                                echo form_input('valor', '', 'size="10" class="campo"');
+                                echo br();
+
+                                echo form_label("Inativo: ");
+                                echo form_input('inativo', '', 'size="50" class="campo" required');
+                                echo br();
+                                
+                                echo form_label("Data Cadastro: ");
+                                echo form_input('data_cadastro', '', 'size="50" class="campo" required');
+                                echo br();
+                                
+                                echo form_label("Numero Serie: ");
+                                echo form_input('numero_serie', '', 'size="50" class="campo" required');
+                                echo br();
+                                
+                                echo form_label("Detalhes: ");
+                                echo form_input('detalhes', '', 'size="50" class="campo" required');
+                                echo br();
+
+                                echo form_label("Genero Codigo: ");
+                                $listagem = array('' => 'Selecione o Genero');
+                                foreach ($genero_codigo as $e) {
+                                    $listagem[$e->codigo] = "$e->codigo - $e->nome";
+                                }
+                                echo form_dropdown('genero_codigo', $listagem, set_value('genero_codigo', @$generoAtual), 'required');
+                                echo br();
+                                
+                                echo form_label("Classificação Codigo: ");
+                                $listagem = array('' => 'Selecione a Classificaçâo');
+                                foreach ($classificacao_codigo as $e) {
+                                    $listagem[$e->codigo] = "$e->codigo - $e->nome";
+                                }
+                                echo form_dropdown('classificacao_codigo', $listagem, set_value('classificacao_codigo', @$classificacaoAtual), 'required');
+                                echo br();
+                                
+                                echo form_label("Tipo Codigo: ");
+                                $listagem = array('' => 'Selecione o Tipo');
+                                foreach ($tipo_codigo as $e) {
+                                    $listagem[$e->codigo] = "$e->codigo - $e->nome";
+                                }
+                                echo form_dropdown('tipo_codigo', $listagem, set_value('tipo_codigo', @$tipoAtual), 'required');
+                                echo br();
+                                
+                                echo form_label("Dias Devolução: ");
+                                echo form_input('dias_devolucao', '', 'size="50" class="campo" required');
+                                echo br();
+
+                                echo form_submit('submit', 'Enviar', 'class="botao1"');
+                                echo form_reset('reset', 'Limpar');
+
+                                echo form_fieldset_close();
+                                echo form_close();
+                                ?>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
